4acaa1326ddc1eabbadea1fa82aca23f3ce32a55
Fernando Falla <2992202650101@ingenieria.usac.edu.gt>
2025-04-20 00:20:07 -0600

    [update] error to ok

--- Backend/tests/app.test.js
+++ Backend/tests/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return Hola Mundo', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hola Mundoo');
+    expect(res.text).toBe('Hola Mundo');
   });
 });
